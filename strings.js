03bb432157eb0223fc15b659f1d834ddc43c1579
Vishal-Sachan <98736529+Vishal-Sachan@users.noreply.github.com>
2022-01-31 15:07:11 +0530

    Play with Strings

---
+++ strings.js
@@ -0,0 +1,29 @@
+console.log('Data Written')
+
+var x = "This is Vishal Sachan ";
+
+var y = 'Node.js Training';
+
+var z = ['Geeks', 'for', 'Geeks'];
+
+console.log(x);
+
+console.log(y);
+
+console.log("Concat Using (+) :", (x + y));
+
+console.log("Concat Using Function :", (x.concat(y)));
+
+console.log("Split string: ", x.split(' '));
+
+console.log("Join string: ", z.join(' : '));
+
+console.log("Char At Index 5: ", x.charAt(5) );
+
+console.log("\n\n"+ typeof x);
+
+console.log(typeof y);
+
+console.log(typeof z);
+
+console.log(z[3]);
