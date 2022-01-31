eeee00845f5a633f6c6aecb32b130f89b88eb049
Vishal-Sachan <98736529+Vishal-Sachan@users.noreply.github.com>
2022-01-31 15:15:18 +0530

    OS module example

---
+++ Modules/OS/os.js
@@ -0,0 +1,21 @@
+var os = require("os");
+
+console.clear();
+
+console.log(os.hostname());
+
+console.log(os.type());
+
+console.log(os.freemem());
+
+console.log(os.totalmem());
+
+console.log(os.networkInterfaces());
+
+console.log(os.tmpdir());
+
+console.log(os.arch());
+
+console.log(os.cpus());
+
+console.log(os.endianness());
